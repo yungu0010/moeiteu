dedce7ddecc2a214421f9b22871af9af3768ff6a
yoondabin <yundabin0608@gmail.com>
2021-11-21 00:24:14 +0900

    완등시 뱃지 발급하는 코드 구현

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const User = require('../src/server/models/user');
-const Follow = require('../src/server/models/follow')
+const Follow = require('../src/server/models/follow');
+const Record = require('../src/server/models/record');
+const Rating = require('../src/server/models/rating');
 let mine;
 
 const search = (req,res,next)=>{
@@ -57,4 +59,33 @@ const unfollow =(req, res, next) => {
     })
 }
 
-module.exports = {search,follow, unfollow};
+const badge = async(req, res, next) => {
+    // 사람 - 산 사이 연관테이블에 기록 생성
+    // 근데 record는 등산할때마다 기록해줘야함
+    // 
+ 
+    Record.create(({
+        temp_altitude: req.body.altitude,
+        user_id = req.body.myId,
+        mountain_id = req.body.mountainId
+    }))
+    .then(() => {
+        console.log("Record completed")
+    })
+    .catch(err => {
+        console.log(err);
+    });
+    
+    // 근데 만들어진 rating 테이블 보면 userId랑 user_id 둘다 있는데 왠지 모르겠음;;;  UserId인거 보면 시퀄라이즈에서 자동으로 생성한 것 같은데 왠지 흠;;
+    // 그리고 관계테이블에 넣을때 create한후에 키?? 각각 테이블에 관계로 넣은 애들도 저렇게 적어도 되는건지 모르겠도;; 일단 써봤는데 테스트를 못해서 어떤지 모르겠듬
+    const result= await Rating.findOne({where: {user_id: req.body.myId, mountain_id:req.body.mountainId}})
+    if (result == null){ // record에 기록이 없다면 기록하기 즉 뱃지 받은거임 평가는 기본값 있길래 안줌
+        Rating.create(({
+            user_id: req.body.myId,
+            mountain_id: req.body.mountainId
+        }))
+    }
+
+}
+
+module.exports = {search,follow, unfollow, badge};

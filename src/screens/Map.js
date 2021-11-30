220040b1533223db106b9ca7e7ccb06004f6e800
yoondabin <yundabin0608@gmail.com>
2021-11-30 12:12:47 +0900

    Map 수정

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,66 +1,91 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
-import {View, Text, StyleSheet, TouchableOpacity, Dimensions} from 'react-native';
-import * as Location from 'expo-location';
-import { long } from 'webidl-conversions';
-
+import {View, StyleSheet, Dimensions} from 'react-native';
+import Geolocation from "react-native-geolocation-service";
 // MapView 사용을 위해 구글에 가서 API 키 만들고 사용어플 안드로이드 - 키제한: 구글 SDK MAP 주고
 // app.json에다가 android에 패키지랑 config 추가
-// react-native-maps 관련 API : https://github.com/react-native-maps/react-native-maps 
-
-// 동적인 어플리케이션을 만드려면 다음을 사용하는게 코딩시 쉬울듯.
 const width = Dimensions.get('window').width
 const height = Dimensions.get('window').height
-let Nlatitude, Nlongtitude
 
 const Map = () => {
+    // react-native-geolocation-service 모듈로 사용자의 현위치 가져올 것
+    // https://dev-yakuza.posstree.com/ko/react-native/react-native-geolocation-service/
+    // https://dev-yakuza.posstree.com/ko/react-native/react-native-maps/
+  
+    const [location, setLocation]=useState(
+        [{ latitude: 37.5642135, 
+            longitude: 127.0016985,
+        }]
+    );
 
-    // 사실 초기값은 읽어온 위도 경도 넣어야 할 것. weathersetting.js에서 읽어온걸..? 넣도록
-    // 사용자 위치추적도 가능함 하고싶다면 사용할 것.
-    const [location, setLocation]=useState({
-        latitude: 37.5642135, 
-        longitude: 127.0016985,
-        latitudeDelta: 0.1, // 얘네 늘이면 지도 확대율 지정됨
-        longitudeDelta: 0.1,
-    })
+    let watchId;
 
-    // onRegionChange위치 변경시 발생이벤트인데 성능저하 우려시 throttle 사용
-    // 변경 완료시 발생하는 이벤트는 onRegionChangeComplete임
+    useEffect(()=>{
+        // watchPosition은 위치추적 요청의 고유 식별자 반환 clearWatch에 이값 넣으면 추적종료 가능
+        // 인자는 3개로 success시, error시, option
+        watchId = Geolocation.watchPosition(
+            (position) => {
+                const{latitude, longitude}=position.coords;
+                setLocation([...location, {latitude, longitude}]);
+            },
+            (error)=>{console.log(error);},
+            {
+                // enableHighAccuracy:true, => 높은 정확도, 대신 느린응답과 전력소모율 증가
+                distanceFilter:1,        // 최소 변화 미터
+                interval:1000,           // active location 업뎃을 위한 interval
+                fastestInterval:500,
+            },
+        );
+    }, [location])
+
+    // 언제 clear 해줄지 의문
+    useEffect(() => {
+        return () => {
+            if(watchId!==null){
+                Geolocation.clearWatch(watchId);
+            }
+        };
+    },[])
+
+    // onRegionChange는 지도에서 위치가 변경될경우 => 사용자의 위치변경을 반영하는게 아닌듯..
     return (
         <View style={styles.container}>
-            <MapView
-                style={styles.map}
-                // + 나중 customMapStyle 변경 json 파일 다운받는 법 알아보기 
-                provider={PROVIDER_GOOGLE}
-                initialRegion={location}
-                onRegionChange={()=>{
-                    setLocation({
-                        latitude: location.latitude,
-                        longitude: location.longitude,
+            {location.length>0 && (
+                <MapView
+                    style={styles.map}
+                    // + 나중 customMapStyle 변경 json 파일 다운받는 법 알아보기 
+                    provider={PROVIDER_GOOGLE}
+                    initialRegion={{ 
+                        latitude: location[0].latitude, 
+                        longitude: location[0].longitude,
                         latitudeDelta: 0.1,
                         longitudeDelta: 0.1
-                    });
-                }}
-            >
-            <Marker
-                coordinate={location}
-                title="현위치"
-            />
-            </MapView>
+                    }}
+                >
+                <Marker
+                    cordinate = {{
+                        latitude: location[location.length-1].latitude,
+                        longitude: location[location.length-1].longitude
+                    }}
+                    title="현위치"
+                />
+                </MapView>
+            )}
         </View>
     )
 }
+
     
 const styles = StyleSheet.create({
     container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
     },
     map: {
-    top: height*0.08,
-    width: width*0.9,
-    height: height*0.7,
+        top: height*0.08,
+        width: width*0.9,
+        height: height*0.7,
     },
 });
 
